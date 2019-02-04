028180cebc2965e4c222280dd9184317c75da7dd
Md. Aminur Rab Ratul <ratularn2@gmail.com>
2019-02-04 00:51:18 -0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+import  matplotlib.pyplot as plt
+import numpy as np
+
+mnist=tf.keras.datasets.mnist #28*28 images of hand-written digits
+
+(x_train,y_train),(x_test,y_test)=mnist.load_data() #unpack data
+
+x_train=tf.keras.utils.normalize(x_train,axis=1)
+x_test=tf.keras.utils.normalize(x_test,axis=1)
+print(x_train[9])
+print(y_train[2])
+plt.imshow(x_train[2],cmap=plt.cm.binary)
+plt.show()
+model=tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128,activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(128,activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(10,activation=tf.nn.softmax))
+model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+model.fit(x_train,y_train,epochs=3)
+val_loss, val_acc = model.evaluate(x_test, y_test)  # evaluate the out of sample data with model
+print('loss: ',val_loss)  # model's loss (error)
+print('acc: ',val_acc)  # model's accuracy
+
+model.save('epic_num_reader.model')
+new_model=tf.keras.models.load_model('epic_num_reader.model')
+predictions=new_model.predict([x_test]) #predict always take list
+print(predictions)
+
+#print(x_train[0])
+#plt.imshow(x_train[0],cmap=plt.cm.binary)
+#plt.show()
+
+print(np.argmax(predictions[0]))
+
+plt.imshow(x_test[0])
+plt.show()
+
+print(y_test[0])
